7dfe610fbdb6d4f5da43053857cf23130489c994
Francy <franoemi07@gmail.com>
2025-02-15 11:43:50 +0100

    continuato login.tsx

--- app/login.tsx
+++ app/login.tsx
@@ -1,23 +1,56 @@
-import { View, Text, SafeAreaView } from 'react-native'
+import {View, Text, SafeAreaView, TextInput, TouchableOpacity} from 'react-native'
 import React from 'react'
-import Background from "@/components/Background";
+import {Link} from "expo-router";
 
 const Login = () => {
   return (
-    <Background>
+    <SafeAreaView className="flex flex-col align-center h-screen mx-12">
 
-      <View className="flex flex-row justify-center border m-5 gap-20">
+      {/* Title */}
+      <View className="flex flex-col justify-center items-left my-10">
+        <View className="gap-y-2">
+          <Text className="text-4xl font-normal tracking-wide uppercase">Benvenuto su</Text>
+          <Text className="text-6xl font-extrabold tracking-widest uppercase">Neura</Text>
+        </View>
+      </View>
 
-        <View className="border">
-          <Text className="text-6xl text-left">Benvenuti</Text>
+      {/* Login field */}
+      <View className="flex flex-col justify-center items-left my-8 gap-y-3.5">
+
+        <View>
+          <Text className="text-3xl font-normal tracking-wide uppercase mx-2">Accedi</Text>
         </View>
-        <View className="border">
-          <Text className="text-6xl text-right">su</Text>
+
+        <View className="border rounded-xl bg-neutral-100 px-3 ">
+          <TextInput placeholder="email">
+          </TextInput>
         </View>
+
+        <View className="border rounded-xl bg-neutral-100 px-3 ">
+          <TextInput placeholder="password">
+          </TextInput>
+        </View>
+
+        <Link className="text-md text-right font-normal tracking-wide uppercase underline" href="/">Password dimenticata?</Link>
+
+        <TouchableOpacity className="bg-black rounded-[20px] px-[29px] py-[9px] shadow-black elevation-xl self-start" >
+          <Text className="text-white font-bold uppercase tracking-widest">Login</Text>
+        </TouchableOpacity>
 
       </View>
 
-    </Background>
+      <View className="flex flex-col bg-black h-[1.5px]" />
+
+      <View className="flex flex-col justify-center items-center my-3">
+        <Text className="text-md tracking-wide uppercase ">
+          login con altro
+        </Text>
+      </View>
+
+
+
+
+    </SafeAreaView>
   )
 }
 export default Login
